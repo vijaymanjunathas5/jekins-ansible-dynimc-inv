4494d6de588fb755e3c40e2b5294e1ce39e34a47
vijaymanjunathas5 <110052997+vijaymanjunathas5@users.noreply.github.com>
2022-09-26 16:45:11 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -2,7 +2,7 @@ provider "aws"{
    region  = "ap-south-1" 
    }
 
-resource "aws_instance" "NewJenj"{
+resource "aws_instance" "NewJenjlo09"{
      count   = 1
      ami     =  "ami-05c8ca4485f8b138a"
      instance_type = "t2.micro"
